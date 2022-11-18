4874559c3e417a13a19a23d66d1eed7fb728c0ac
jhan daza <jhand@keoworld.com>
2022-11-18 12:00:21 -0500

    add chromedriver.exe

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-import chromedriver_autoinstaller
+#import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install() 
+#chromedriver_autoinstaller.install() 
 
 chromeOptions = Options()
-chromeOptions.headless = True
+#chromeOptions.headless = True
 chromeOptions.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/95.0.4638.54 Safari/537.36")
 #chromeOptions.add_extension(r'C:\Users\jhand\Buster-Captcha-Solver-for-Humans.crx')
 
-driver = webdriver.Chrome(options=chromeOptions)
+#driver = webdriver.Chrome(options=chromeOptions)
+driver = webdriver.Chrome('./chromedriver.exe', options=chromeOptions)
 
 now = datetime.now() 
 year_month_day = now.strftime("%Y-%m-%d")
@@ -32,7 +33,7 @@ driver.get(url)
 driver.maximize_window()
 
 title = driver.find_element(By.XPATH, "//*[@id='comp-kwclfsi1']/h2[1]").text
-print(title)
+print("Title: ", title)
 
 
 
